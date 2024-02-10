b3148f13989f31a459257f0d5ba2589d20506af1
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-02-10 23:18:11 +0530

    refactor: run prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: true,
-    singleQuote: false,
-    useTabs: false,
-    bracketSpacing: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  bracketSpacing: true,
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    watch: true,
-    devtool: "inline-source-map",
+  mode: "development",
+  watch: true,
+  devtool: "inline-source-map",
 });
